116d7ce856d9d7ad6abb53bd6ce1e2f3457baee6
Tiago Vitorino <brother_87_22@hotmail.com>
2019-05-11 14:30:43 -0300

    first commit

---
+++ src/controller/TelaAux.java
@@ -0,0 +1,34 @@
+package controller;
+
+import view.MainView;
+
+/**
+ * Esta classe trabalha com a classe TelaTemplate
+ * Serve para ajudar a TelaTemplate executar suas ações contribuindo 
+ * no reaproveitamento de ambas as classes
+ * É nesta classe em que irei alterar os métodos da classe TelaTemplate
+ * @author Tiago Vitorino
+ * @since 17/02/2019
+ */
+public abstract class TelaAux extends MainView{	
+
+	/**
+	 * Método executado ao mover o dedo na tela da direita para esquerda
+	 */
+	public static void moverDireitaParaEsquerda() {
+		
+		//System.out.println("Fui para esquerda");
+		carregarIdeia();
+		mudarACor();
+	}
+
+	/**
+	 * Método executado ao mover o dedo na tela da esquerda para direita
+	 */
+	public static void moverEsquerdaParaDireita() {
+	
+		//System.out.println("Fui para direita");
+		carregarIdeiaAnterior();
+		mudarACor();
+	}		
+}
